892710a0626cea44eb9b2e96ef1c784ff4544c18
Dominux <kir.gimranov@gmail.com>
2022-01-31 01:48:50 +0300

    Added PORT getting from env

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use actix_cors::Cors;
 use actix_web::{web, App, HttpServer};
 use reqwest;
 
+
+#[allow(unused_must_use)]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+		let cors = Cors::default().allow_any_origin();
+        App::new()
+            .wrap(cors)
+            .route("/{url:.*}", web::get().to(cors_proxy_get))
+    })
+		.bind(("127.0.0.1", get_port()))?
+		.run()
+		.await;
+
+    Ok(())
+}
+
 async fn cors_proxy_get(url: web::Path<String>) -> String {
 	// TODO: make it better 
 	println!("Requesting url: {}", &url);
@@ -13,18 +30,9 @@ async fn cors_proxy_get(url: web::Path<String>) -> String {
         .expect("msg")
 }
 
-#[allow(unused_must_use)]
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-		let cors = Cors::default().allow_any_origin();
-        App::new()
-            .wrap(cors)
-            .route("/{url:.*}", web::get().to(cors_proxy_get))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await;
-
-    Ok(())
+fn get_port() -> u16 {
+	std::env::var("PORT")
+		.unwrap_or("8080".to_string())
+		.parse()
+		.unwrap()
 }
