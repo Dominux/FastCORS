aac6c8cec80132d78ef3ad17848d3631332d7f12
Dominux <kir.gimranov@gmail.com>
2022-01-31 18:28:38 +0300

    Bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use actix_cors::Cors;
-use actix_web::{web, App, HttpServer, HttpRequest, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use reqwest;
-
 
 #[allow(unused_must_use)]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-		println!("Server is running on port {}", get_port());
-		let cors = Cors::default().allow_any_origin();
+        println!("Server is running on port {}", get_port());
+        let cors = Cors::default().allow_any_origin();
         App::new()
             .wrap(cors)
             .route("/{path:.*}", web::get().to(cors_proxy_get))
     })
-		.bind(("0.0.0.0", get_port()))?
-		.run()
-		.await;
+    .bind(("0.0.0.0", get_port()))?
+    .run()
+    .await;
 
     Ok(())
 }
 
 async fn cors_proxy_get(request: HttpRequest) -> impl Responder {
-	// Removing first slash from relative path
-	let request_path = &request.path()[1..request.path().len() - 1];
-	let url = format!("{}?{}", request_path, request.query_string());
+    // Removing first slash from relative path
+    let request_path = &request.path()[1..request.path().len()];
+    let url = format!("{}?{}", request_path, request.query_string());
 
-	// TODO: make it better 
-	println!("Requesting url: {}", &url);
+    // TODO: make it better
+    println!("Requesting url: {}", &url);
     reqwest::get(url)
         .await
         .expect("mslolg")
@@ -36,8 +35,8 @@ async fn cors_proxy_get(request: HttpRequest) -> impl Responder {
 }
 
 fn get_port() -> u16 {
-	std::env::var("PORT")
-		.unwrap_or("8080".to_string())
-		.parse()
-		.unwrap()
+    std::env::var("PORT")
+        .unwrap_or("8000".to_string())
+        .parse()
+        .unwrap()
 }
